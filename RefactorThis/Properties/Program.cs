f5881e9e0ce32538184a94c78a909622688846e7
SarahLiu2201 <78516325+SarahLiu2201@users.noreply.github.com>
2021-03-09 08:53:34 +1100

    Add files via upload

---
+++ RefactorThis/Properties/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace RefactorThis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+           .AddCommandLine(args)
+           .Build();
+
+            try
+            {
+                var myHost = new WebHostBuilder()
+                    .UseConfiguration(config)                    
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseKestrel()                    
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    ;
+                return myHost;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+    }
+}
